eeae29ae3cf7f2a2f27c15de1a153c362dcc6fd1
yannivt <yanivt@traxretail.com>
2021-01-08 11:30:45 +0200

    use ajv keywords

--- src/utils/util.ts
+++ src/utils/util.ts
@@ -1,12 +1,13 @@
-import { JSONSchemaType as _JSONSchemaType } from "ajv";
+// TODO change after fastify migration
+//import { JSONSchemaType as _JSONSchemaType } from "ajv";
+//export declare type JSONSchemaType<T> = NestedPartial<_JSONSchemaType<T>>;
+export declare type JSONSchemaType<T> = any;
 
 export const toSnack = key => key.replace(/([A-Z])/g, $1 => "-" + $1.toLowerCase()).replace(/^-/, "");
 
 type NestedPartial<T> = {
   [K in keyof T]?: T[K] extends Array<infer R> ? Array<NestedPartial<R>> : T[K] extends unknown ? unknown : NestedPartial<T[K]>;
 };
-
-export declare type JSONSchemaType<T> = NestedPartial<_JSONSchemaType<T>>;
 
 export declare type ValueProvider = { provide: any; useValue: any };
 export declare type Provider = { new (...args) } | ValueProvider;
