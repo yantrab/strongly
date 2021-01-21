39321f0695dacad65a02338d5a9efb964a716c5c
yannivt <yanivt@traxretail.com>
2021-01-21 08:33:45 +0200

    fix bug in di service

--- src/server/di/di.service.spec.ts
+++ src/server/di/di.service.spec.ts
@@ -1,7 +1,12 @@
 import { suite, test } from "@testdeck/jest";
 import { DIService } from "./di.service";
 import "reflect-metadata";
+import { ServerFactory } from "../server";
+let counter = 0;
 export class someService {
+  constructor() {
+    counter++;
+  }
   someFunction() {}
 }
 
@@ -13,6 +18,10 @@ export class someService3 {}
 export class someController5 {
   constructor(private a: someService, private b: someService2) {}
 }
+
+export class someController6 {
+  constructor(private a: someService) {}
+}
 @suite
 class diTests {
   @test
@@ -21,5 +30,9 @@ class diTests {
 
     const d = await diService.getDependencies(someController5);
     expect(Object.values(d).length).toBe(2);
+  }
+  @test("should create only one someService service") async di2() {
+    await ServerFactory.create({ controllers: [someController5, someController6] });
+    expect(counter).toEqual(1);
   }
 }
